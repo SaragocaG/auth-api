5b62e2a828297b2bf3b4994cd2c8ff8a6839f8d6
SARAGOCA Glaydson <glaydson.saragoca@gmail.com>
2020-09-16 01:28:54 -0300

    rename routes folder

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const router = express.Router();
+const routes = express.Router();
 const authController = require('../controller/authController');
 
-router.use('/auth', authController);
+routes.use('/auth', authController);
 
-module.exports = router;
+module.exports = routes;
